c3cdb880b71afbb3c0e62dd20dc0a2edcd1a35e2
Jovanni Schneider <jovanni.schneider@usach.cl>
2022-10-31 00:32:47 -0300

    version final

---
+++ entregable/year.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+typedef struct year{
+    int access;
+    int year;
+    char moreExpensive[100];
+    float priceEx;
+    char cheaper[100];
+    float  priceCh;
+    char free[1500];
+    int totalGamesYear;
+    float win;
+    float mac;
+    float lix;
+    float acum;
+}year;
